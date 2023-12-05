4acef5fe183ef086274806594f5aa281fc00bef4
feliperyder <147994564+feliperyder@users.noreply.github.com>
2023-12-05 21:40:54 +0000

    Initial code written for scores.js

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,7 +1,30 @@
 // scores.js
-
 // Record high scores
-
-// Load high scores from highscores.html
-
-// Replace high score
+function saveHighScore(initials, score) {
+    // Check that initials are not empty
+    if (initials !== '') {
+      // Retrieve high scores from localStorage
+      const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+      // Add the current score and initials to high scores
+      highScores.push({ initials, score });
+      // Save updated high scores to localStorage
+      localStorage.setItem('highScores', JSON.stringify(highScores));
+    }
+  }
+  
+  // Load high scores from highscores.html
+  function loadHighScores() {
+    // Retrieve high scores from localStorage or initialize an empty array
+    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+    // Sort high scores in descending order
+    highScores.sort((a, b) => b.score - a.score);
+    // Display high scores on the highscores.html page
+    highscoresList.innerHTML = '';
+    const highscoresList = document.getElementById('highscores');
+    highScores.forEach((entry, index) => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${index + 1}. ${entry.initials}: ${entry.score}`;
+      highscoresList.appendChild(listItem);
+    });
+  }
+  
